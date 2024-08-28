ded2e150b9a4f4b8599f2e0e91fcebba000167c4
AVRSANAND <avrsanand2003@gmail.com>
2024-08-28 23:50:47 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+
+def jinaai_readerapi(url):
+    response = requests.get("https://r.jina.ai/" + url_proxies)
+    return response.text
+
+url_proxies = "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text"
+
+data = jinaai_readerapi(url_proxies)
+print(data)
+
+
+# proxies = open("proxies.txt", "r").read().strip().split("\n")
+
+def get(url, proxy):
+    """
+    Sends a GET request to the given url using given proxy server.
+    The proxy server is used without SSL, so the URL should be HTTP.
+
+    Args:
+        url - string: HTTP URL to send the GET request with proxy
+        proxy - string: proxy server in the form of {ip}:{port} to use while sending the request
+    Returns:
+        Response of the server if the request sent successfully. Returns `None` otherwise.
+
+    """
+    try:
+        r = requests.get(url, proxies={"http": f"http://{proxy}"})
+        if r.status_code < 400:  # client-side and server-side error codes are above 400
+            return r
+        else:
+            print(r.status_code)
+    except Exception as e:
+        print(e)
+
+    return None
+
+
+def check_proxy(proxy):
+    """
+    Checks the proxy server by sending a GET request to httpbin.
+    Returns False if there is an error from the `get` function
+    """
+
+    return get("http://httpbin.org/ip", proxy) is not None
+
+
+# available_proxies = list(filter(check_proxy, proxies))
